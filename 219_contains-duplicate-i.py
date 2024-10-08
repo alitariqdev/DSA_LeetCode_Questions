8d9c9d5196d54ce1c7e276b08c6d29fd2b1e878e
Ali Tariq <74070552+alitariqdev@users.noreply.github.com>
2024-10-08 07:53:35 +0500

    219

---
+++ 219_contains-duplicate-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        dic = {}
+
+        for index, value in enumerate(nums):
+            if value in dic and abs(index - dic[value]) <=k:
+                return True
+            else:
+                dic[value] = index
+        return False
+        
