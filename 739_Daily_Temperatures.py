e8e68e228eda665ff3a1e613d7f0b3a84254422d
Ali Tariq <74070552+alitariqdev@users.noreply.github.com>
2025-03-04 08:21:02 +0500

    Added Optimized Solution for Problem_739

--- 739_Daily_Temperatures.py
+++ 739_Daily_Temperatures.py
@@ -1,5 +1,5 @@
 # Solution Using Two Pointer Approach
-def DailyTemperatures(T):
+def Daily_Temperatures_Two_Pointers_Approach(T):
     n = len(T)
     result = [0] * n
 
@@ -14,9 +14,25 @@ def DailyTemperatures(T):
     return result
 
 
-
-
-
-
 T = [73,74,75,71,69,72,76,73]
-print(DailyTemperatures(T))
+print(Daily_Temperatures_Two_Pointers_Approach(T))
+
+
+
+
+# Solution Using Monotonic Stack 
+
+def dailyTemperatures(T):
+        s = []
+        r = [0] * len(T)
+
+        for i in range (len(T)):
+            while s and T[i] > T[s[-1]]:
+                index = s.pop()
+                r[index] = i - index
+            s.append(i)
+        
+        return r
+
+
+print(dailyTemperatures(T))
