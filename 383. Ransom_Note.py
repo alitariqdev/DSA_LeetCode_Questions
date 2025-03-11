c95bce0013ff1abd6f69a00a5f59c6b9bd012b4e
Ali Tariq <74070552+alitariqdev@users.noreply.github.com>
2025-03-12 02:24:25 +0500

    Create 383. Ransom_Note.py

---
+++ 383. Ransom_Note.py
@@ -0,0 +1,15 @@
+from collections import Counter
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+
+        r = Counter(ransomNote)
+        m = Counter(magazine)
+
+
+        for char, count in r.items():
+            if count > m[char]:
+                return False
+
+        return True
+
+        
