cedb57fa029d106c7187487acf2621d9a2ac9129
Ali Tariq <74070552+alitariqdev@users.noreply.github.com>
2025-03-13 23:32:26 +0500

    Create 2529. Maximum_Count_of _Positive_Integer_and_Negative_Integer.py

---
+++ 2529. Maximum_Count_of _Positive_Integer_and_Negative_Integer.py
@@ -0,0 +1,19 @@
+# Brute Forece Solution o(n)
+
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+
+        pos_count = 0
+        neg_count = 0 
+
+        for i in nums:
+            if i > 0:
+                pos_count += 1
+            elif i < 0:
+                neg_count += 1
+            else:
+                continue 
+        return max(pos_count, neg_count)       
+
+
+# can you solve it in O(log n )
