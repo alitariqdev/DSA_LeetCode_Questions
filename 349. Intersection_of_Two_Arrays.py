8908ab368807768499c8c0659faa463e2ba7b611
Ali Tariq <74070552+alitariqdev@users.noreply.github.com>
2025-05-12 10:23:03 +0500

    Update 349. Intersection_of_Two_Arrays.py

--- 349. Intersection_of_Two_Arrays.py
+++ 349. Intersection_of_Two_Arrays.py
@@ -9,4 +9,23 @@ class Solution:
                 arr.append(nums2[i])
             
         return arr
-        
+
+
+
+
+
+
+
+
+# second solution
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = []
+        a = set(nums1)
+        b = set(nums2)
+        for i in a:
+            if i in b:
+                res.append(i)
+        return res
