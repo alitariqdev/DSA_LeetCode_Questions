58239226cb647447ed3e0b8816a67c928317738f
Ali Tariq <74070552+alitariqdev@users.noreply.github.com>
2026-05-19 01:02:33 -0500

    Create 1448. Count Good Nodes in Binary Tree.py

---
+++ 1448. Count Good Nodes in Binary Tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        self.count = 0 
+        def dfs(root,max_so_far):
+            
+            if not root:
+                return
+            
+            if root.val >= max_so_far:
+                max_so_far = root.val
+                self.count += 1
+
+            dfs(root.left, max_so_far)
+            dfs(root.right, max_so_far)
+        dfs(root,root.val)
+        return self.count 
+
+        
