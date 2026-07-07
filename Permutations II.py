9f297436cf145296541030c64f32f043a2278684
Ali Tariq <74070552+alitariqdev@users.noreply.github.com>
2026-07-07 18:20:57 -0500

    Create Permutations II.py

---
+++ Permutations II.py
@@ -0,0 +1,34 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        
+        n = len(nums)
+        curr = []
+        used = [False] * n 
+        output = []
+        def dfs():
+            if len(curr) == n:
+                output.append(curr.copy())
+                return
+
+
+            for i in range(n):
+                if not used[i]:
+                    continue
+
+                if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
+                    continue
+
+                used[i] = True
+                curr.append(nums[i])
+                dfs()
+                
+                used[i] = False
+                curr.pop()
+                
+        dfs()
+        return output 
+
+
+        
+        
